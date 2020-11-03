ccfbe08ebc7284c0096f51e0f4c16f8f768ef0c9
Riccardo Calligaro <riccardocalligaro@gmail.com>
2020-11-03 21:40:46 +0100

    Initial Commit

---
+++ JsonPlaceholder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JsonPlaceholder
+//
+//  Created by Riccardo on 03/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
